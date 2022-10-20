e230e6b5e2b7dada154ac3095cf1a1ce7aaabb42
Thiago Draeger <thiagodraeger123@gmail.com>
2022-10-20 08:09:46 -0300

    aula 13

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Autor, Categoria, Editora, Livro
 from core.serializers import CategoriaSerializer, EditoraSerializer, LivroSerializer, AutorSerializer
@@ -6,6 +7,8 @@ from core.serializers import CategoriaSerializer, EditoraSerializer, LivroSerial
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticated]  
+    
 
 
 class EditoraViewSet(ModelViewSet):
